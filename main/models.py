3eda8ae0b7e1560677d521cbf656cecc31b6cc33
ZakiyDaulay <zakiydaulay@gmail.com>
2024-09-18 10:43:58 +0700

    finished tutorial 3

--- main/models.py
+++ main/models.py
@@ -1,11 +1,13 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class MoodEntry(models.Model):
     mood = models.CharField(max_length=255)
     time = models.DateField(auto_now_add=True)
     feelings = models.TextField()
     mood_intensity = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     @property
     def is_mood_strong(self):
