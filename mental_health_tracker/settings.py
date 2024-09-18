1f978d3fd833be6c24168c8387ca579d3f7ca8eb
ZakiyDaulay <zakiydaulay@gmail.com>
2024-09-18 10:49:07 +0700

    added a PWS deployment link

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -35,7 +35,6 @@ CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://pbp.cs.ui.
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
