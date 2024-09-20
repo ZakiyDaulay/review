d996323b610571a153e617e8a26d9f87d763b9b4
ZakiyDaulay <zakiydaulay@gmail.com>
2024-09-20 23:41:22 +0700

    modified all of the tests in test.py so that it will work for the quiz

--- main/tests.py
+++ main/tests.py
@@ -1,31 +1,46 @@
-from django.test import TestCase, Client
+from django.test import TestCase
+from django.contrib.auth.models import User
 from django.utils import timezone
 from .models import MoodEntry
 
 class MainTest(TestCase):
-    def test_main_url_is_exist(self):
-        response = Client().get('')
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(username='testuser', password='12345')
+
+    def test_main_url_exists(self):
+        # Log in the user and set the cookie
+        self.client.login(username='testuser', password='12345')
+        self.client.cookies['last_login'] = '2024-09-20T12:00:00Z'  # Example timestamp
+        response = self.client.get('')
         self.assertEqual(response.status_code, 200)
 
     def test_main_using_main_template(self):
-        response = Client().get('')
+        # Log in the user and set the cookie
+        self.client.login(username='testuser', password='12345')
+        self.client.cookies['last_login'] = '2024-09-20T12:00:00Z'
+        response = self.client.get('')
         self.assertTemplateUsed(response, 'main.html')
 
     def test_nonexistent_page(self):
-        response = Client().get('/skibidi/')
+        response = self.client.get('/skibidi/')
         self.assertEqual(response.status_code, 404)
 
     def test_strong_mood_user(self):
         now = timezone.now()
         mood = MoodEntry.objects.create(
-          mood="Happy",
-          time = now,
-          feelings = "I'm happy, even though my clothes are soaked from the rain :(",
-          mood_intensity = 8,
+            user=self.user,  # Ensure mood entry is associated with the test user
+            mood="Happy",
+            time=now,
+            feelings="I'm happy, even though my clothes are soaked from the rain :(",
+            mood_intensity=8,
         )
         self.assertTrue(mood.is_mood_strong)
 
     def test_main_template_uses_correct_page_title(self):
-        response: Client().get("/")
+        # Log in the user and set the cookie
+        self.client.login(username='testuser', password='12345')
+        self.client.cookies['last_login'] = '2024-09-20T12:00:00Z'
+        response = self.client.get("/")
         html_response = response.content.decode("utf8")
-        self.assertIn("PBD Mental Health Tracker",html_response)
+        self.assertIn("PBD Mental Health Tracker", html_response)
