950444fe7c517aafdd1ae8a9569e848d34caf9ae
ZakiyDaulay <zakiydaulay@gmail.com>
2024-09-21 01:18:43 +0700

    added functional_tests.py and modified some other files to make quiz 4 work with the changes made after tutorial 3

--- main/views.py
+++ main/views.py
@@ -20,7 +20,7 @@ def show_main(request):
         'class': 'PBP KKI',
         'npm': '2306123456',
         'mood_entries': mood_entries,
-        'last_login':request.COOKIES['last_login']
+        'last_login': request.COOKIES.get('last_login', 'No recent login information')
     }
 
     return render(request, "main.html", context)
