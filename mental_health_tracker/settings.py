8cb3c14cc4b073716286219844bd2e1319c77113
ZakiyDaulay <zakiydaulay@gmail.com>
2024-09-20 22:16:47 +0700

    added failing test for quiz

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fm!6+3j6t@bmd36!849#yw1^)3x4)=rv@&#tbc=ay81n^1jvv2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1","zakiy-makarim-mentalhealthtracker.pbp.cs.ui.ac.id"]
